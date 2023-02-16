eff737fc1931a9c3aeefc7d4be50e818a4bf5ab5
= <116816976+graceksts@users.noreply.github.com>
2023-02-16 18:47:59 +0700

    Update HanoiTower.py

--- Lab6_3/HanoiTower.py
+++ Lab6_3/HanoiTower.py
@@ -32,7 +32,7 @@ class Disk(object):
     def cleardisk(self):
         self.rect.setheading(0)
         self.rect.clear()
-        # self.rect.done()
+        self.rect.done()
 
 class Pole(object):
     def __init__(self, name="", xpos=0, ypos=0, thick=10, length=100):
@@ -77,5 +77,31 @@ myPole = Pole()
 myPole.showpole()
 
 class Hanoi(object):
-    pass
+    def __init__(self, n=3, start="A", workspace="B", destination="C"):
+        self.startup = Pole(start,0,0)
+        self.workspacep = Pole(workspace,150,0)
+        self.destinationp = Pole(destination, 300, 0)
+        self.startp.showpole()
+        self.workspacep.showpole()
+        self.destinationp.showpole()
+        for i in range(n):
+            self.startp.pushdis(Disk("d" + str(i), 0, i*150,20,(n-i)*30))
+
+    def move_disk(self, start, destination):
+        disk = start.popdisk()
+        destination.pushdisk(disk)
+
+    def move_tower(self,n,s,d,w):
+        if n == 1:
+            self.move_disk(s,d)
+        else:
+            self.move_tower(n-1,s,w,d)
+            self.move_disk(s,d)
+            self.move_tower(n-1,w,d,s)
+
+    def solve(self):
+        self.move_tower(3, self.startp, self.destination, self.workspacep)
+
+h = Hanoi()
+h.solve()
 
